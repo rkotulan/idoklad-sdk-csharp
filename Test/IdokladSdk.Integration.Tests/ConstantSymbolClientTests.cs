e779f637e17c1694418f07ce08bc6d61778bcc29
Miroslav Holec <mirek.holec@hotmail.com>
2016-01-26 18:10:04 +0100

    Added ConstantSymbols tests

---
+++ Test/IdokladSdk.Integration.Tests/ConstantSymbolClientTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using IdokladSdk.ApiFilters;
+using NUnit.Framework;
+
+namespace IdokladSdk.Integration.Tests
+{
+    [TestFixture]
+    public class ConstantSymbolClientTests : IntegrationTestBase
+    {
+        [Test]
+        public void IntegrationSteps()
+        {
+            Changes();
+            int id = ConstantSymbols();
+            ConstantSymbol(id);
+        }
+
+        private void ConstantSymbol(int bankId)
+        {
+            // Act
+            var result = ApiExplorer.ConstantSymbols.ConstantSymbol(bankId);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(bankId, result.Id);
+        }
+
+        private int ConstantSymbols()
+        {
+            // Act
+            var result = ApiExplorer.ConstantSymbols.ConstantSymbols();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Data);
+            Assert.That(result.Data.Count, Is.GreaterThan(0));
+
+            return result.Data.FirstOrDefault().Id;
+        }
+
+        private void Changes()
+        {
+            // Act
+            var result = ApiExplorer.ConstantSymbols.Changes(new ChangeFilter());
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
