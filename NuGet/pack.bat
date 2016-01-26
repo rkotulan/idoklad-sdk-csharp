e7f6ad90ffda50373e5801f179aac7960845ad62
Miroslav Holec <mirek.holec@hotmail.com>
2016-01-26 18:09:43 +0100

    Added NuGetPackage

---
+++ NuGet/pack.bat
@@ -0,0 +1,1 @@
+nuget.exe pack %~dp0\..\Src\Idoklad\IdokladSdk.csproj -Build -Properties Configuration=Release
---
+++ Test/IdokladSdk.Integration.Tests/DocumentAddressClientTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using IdokladSdk.ApiModels;
+using NUnit.Framework;
+
+namespace IdokladSdk.Integration.Tests
+{
+    [TestFixture]
+    public class DocumentAddressClientTests : IntegrationTestBase
+    {
+        [Test]
+        public void IntegrationSteps()
+        {
+            var address = DocumentAddress();
+            Update(address);
+        }
+
+        private DocumentAddress DocumentAddress()
+        {
+            // Act
+            var invoices = ApiExplorer.IssuedInvoices.IssuedInvoices();
+            int addressId = invoices.Data.FirstOrDefault().PurchaserDocumentAddressId;
+            var result = ApiExplorer.DocumentAddresses.DocumentAddress(addressId);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(addressId, result.Id);
+
+            return result;
+        }
+
+        private void Update(DocumentAddress address)
+        {
+            // Arrange
+            string random = new Random().Next(111111, 999999).ToString(CultureInfo.InvariantCulture);
+            address.Firstname = random;
+
+            // Act
+            var result = ApiExplorer.DocumentAddresses.Update(address.Id, address);
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Firstname, Is.EqualTo(random));
+        }
+    }
+}
