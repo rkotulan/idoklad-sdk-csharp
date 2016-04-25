d1c9a0db018420db200d9864701105775ac071e9
Miroslav Holec <mirek.holec@hotmail.com>
2016-04-25 17:33:03 +0200

    Added support for OAuth 2.0 Client Credential flow

---
+++ Src/Idoklad/Clients/OAuth2Client.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Security.Authentication;
+using IdokladSdk.Extensions;
+using RestSharp;
+
+namespace IdokladSdk.Clients
+{
+    /// <summary>
+    /// Provides authentication methods
+    /// </summary>
+    public class OAuth2Client
+    {
+        private const string ResourceUrl = "https://idokladauthserver.azurewebsites.net/identity/connect/token";
+
+        private readonly string _clientId;
+        private readonly string _clientSecret;
+
+        public OAuth2Client(string clientId, string clientSecret)
+        {
+            if (clientId.IsNullOrEmpty() || clientSecret.IsNullOrEmpty())
+            {
+                throw new ArgumentException("client_id and cliend_secret must be provided");
+            }
+
+            _clientId = clientId;
+            _clientSecret = clientSecret;
+        }
+
+        /// <summary>
+        /// Returns secure token needed for authorization
+        /// </summary>
+        public string GetSecureToken()
+        {
+            var client = new RestClient(ResourceUrl);
+            var authRequest = new RestRequest(Method.POST);
+
+            authRequest.AddParameter("content-type", "application/x-www-form-urlencoded");
+            authRequest.AddParameter("grant_type", "client_credentials");
+            authRequest.AddParameter("client_id", _clientId);
+            authRequest.AddParameter("client_secret", _clientSecret);
+            authRequest.AddParameter("scope", "idoklad_api");
+
+            IRestResponse authReponse = client.Execute(authRequest);
+
+            string token = authReponse.Content;
+
+            if (token.IsNullOrEmpty())
+            {
+                throw new AuthenticationException("Authentication failed. Access token has not been obtained.");
+            }
+
+            return token.RemoveQuotationMarks();
+        }
+    }
+}
--- Test/IdokladSdk.Integration.Tests/Auth.cs
+++ Test/IdokladSdk.Integration.Tests/Auth.cs
@@ -2,6 +2,6 @@
 {
     public static class Auth
     {
-        public const string AuthKey = "";
+        public const string AuthKey = "adfd6c334752ffc345264ee2dbba72f26f5f333c18d3d80570219a911013931c";
     }
 }
