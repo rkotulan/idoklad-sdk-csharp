32b491cb18c3e0a96e06473132c1ccec8a2d233c
Miroslav Holec <mirek.holec@hotmail.com>
2016-05-05 20:48:23 +0200

    Created new version 1.0.6 for CI build. Ready for deploy.

---
+++ NuGet/pack-build.bat
@@ -0,0 +1,19 @@
+REM this file is using by build server
+REM it must create unnecessary folders and copy all assemblies for nuspec file
+
+REM delete old packages
+rd package
+rd build
+
+REM create folders for target frameworks
+if not exist build mkdir build
+if not exist package\lib\net45 mkdir package\lib\net45
+if not exist package\lib\net451 mkdir package\lib\net451
+if not exist package\lib\net452 mkdir package\lib\net452
+if not exist package\lib\net46 mkdir package\lib\net46
+
+REM copy assemblies to package folder
+copy %~dp0\..\Src\Idoklad\bin\Release\IdokladSdk.dll package\lib\net45
+copy %~dp0\..\Src\IdokladSdk.Net451\bin\Release\IdokladSdk.dll package\lib\net451
+copy %~dp0\..\Src\IdokladSdk.Net452\bin\Release\IdokladSdk.dll package\lib\net452
+copy %~dp0\..\Src\IdokladSdk.Net46\bin\Release\IdokladSdk.dll package\lib\net46
